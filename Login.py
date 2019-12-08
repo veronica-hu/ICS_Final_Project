ff2f75d3f705e9b61c7e43c0ea140fb6efe13b13
veronica-hu <58297469+veronica-hu@users.noreply.github.com>
2019-12-08 16:35:54 +0800

    Update Login.py

--- Login.py
+++ Login.py
@@ -2,7 +2,7 @@ from tkinter import *
 import tkinter.messagebox
 
 class LoginPanel:
-    def __init__(self):
+    def __init__(self, login_func, reg_func, close_callback):
         self.user = None
         self.key = None
         self.login_frame = None
@@ -11,14 +11,13 @@ class LoginPanel:
         self.login_func = login_func
         self.reg_func = reg_func
         self.close_callback = close_callback
-
-
-
-
         
     def show(self):
         self.login_frame = Tk()
         self.login_frame.configure(background = "#333333")#原：#333333
+        self.login_frame.protocol("WM_DELETE_WINDOW", self.close_callback)
+        
+        #窗口尺寸待修改
         screen_width = self.login_frame.winfo_screenwidth()
         screen_height = self.login_frame.winfo_screenheight()
         width = 400
@@ -26,7 +25,8 @@ class LoginPanel:
         gm_str = "%dx%d+%d+%d" % (width, height, (screen_width - width) / 2,
                                   (screen_height - 1.2 * height) / 2)
         self.login_frame.geometry(gm_str)
-        self.login_frame.title("登录")
+        
+        self.login_frame.title("Login")
         self.login_frame.resizable(width=False, height=False)
 
         title_lable = Label(self.login_frame, text="ICS Chat - LogIn", font=("Helvetica", 16),
@@ -46,23 +46,15 @@ class LoginPanel:
 
         Entry(form_frame, textvariable=self.key, show="*", bg="#e3e3e3", width=30).grid(row = 2, column = 1)
       
-        
         #Button Frame
         btn_frame = Frame(self.login_frame, bg = '#333333')
         btn_frame.grid(row = 3,column = 0)
         self.btn_login = Button(btn_frame, text="Log in", bg="lightgreen", fg="black", width=15,
-                              font=('Helvetica', 11), command=self.login).grid(row = 5, column = 1)
+                              font=('Helvetica', 11), command=self.login_func).grid(row = 5, column = 1)
         self.btn_reg = Button(btn_frame, text = 'Sign up',bg = 'lightgreen',fg = 'black',width = 15,
-                              font = ('Helvetica',11), command = None).grid(row = 6, column = 1)
+                              font = ('Helvetica',11), command = self.reg_func).grid(row = 6, column = 1)
        
-
-        
         self.login_frame.mainloop()
-        
-        
-        
-    def login(self):
-        tkinter.messagebox.showinfo('Reminder','successfully loged in!')
     
     def close(self):
         if self.login_frame == None:
