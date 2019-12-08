a69f9409249b5fbcdd8c6e475cf20af5fd22995b
坦克 <58317222+SZTankWang@users.noreply.github.com>
2019-12-09 00:19:29 +0800

    Update Login.py

--- Login.py
+++ Login.py
@@ -18,11 +18,11 @@ class LoginPanel:
         
         self.login_frame.protocol("WM_DELETE_WINDOW", self.close_callback)
         
-        #窗口尺寸待修改
+        #窗口尺寸已修改
         screen_width = self.login_frame.winfo_screenwidth()
         screen_height = self.login_frame.winfo_screenheight()
-        width = 400
-        height = 300
+        width = 500
+        height = 400
         gm_str = "%dx%d+%d+%d" % (width, height, (screen_width - width) / 2,
                                   (screen_height - 1.2 * height) / 2)
         self.login_frame.geometry(gm_str)
@@ -32,28 +32,28 @@ class LoginPanel:
 
         title_lable = Label(self.login_frame, text="ICS Chat - LogIn", font=("Helvetica", 16),
                             fg="white", bg="#555555")
-        title_lable.grid(row = 0,column = 0)
+        title_lable.pack(ipady = 10, fill = X)
         
         #登陆表单frame
         form_frame = Frame(self.login_frame,bg = '#222222')#原：#222222
-        form_frame.grid(row = 1, column = 0)              
+        form_frame.pack(fill=X, padx=20, pady=10)                   
         
-        Label(form_frame, text="User：", font=("Helvetica", 12), bg="#222222", fg="white", width = 10, height = 3).grid(row = 1, column = 0) #原： bg="#222222",fg="white"          
-        Label(form_frame, text="Password：", font=("Helvetica", 12), bg="#222222", fg="white", width = 10, height = 3).grid(row = 2, column = 0)
+        Label(form_frame, text="User：", font=("Helvetica", 12), bg="#222222", fg="white", width = 10, height = 3).grid(row=0, column=1, pady=20) #原： bg="#222222",fg="white"          
+        Label(form_frame, text="Password：", font=("Helvetica", 12), bg="#222222", fg="white", width = 10, height = 3).grid(row=1, column=1, pady=20)
             
         self.user = StringVar()
         self.key = StringVar()
-        Entry(form_frame, textvariable=self.user, bg="#e3e3e3", width=30).grid(row = 1, column = 1)
+        Entry(form_frame, textvariable=self.user, bg="#e3e3e3", width=30).grid(row=0, column=2, ipady=1)
 
-        Entry(form_frame, textvariable=self.key, show="*", bg="#e3e3e3", width=30).grid(row = 2, column = 1)
+        Entry(form_frame, textvariable=self.key, show="*", bg="#e3e3e3", width=30).grid(row=1, column=2, ipady=1)
       
         #Button Frame
         btn_frame = Frame(self.login_frame, bg = '#333333')
-        btn_frame.grid(row = 3,column = 0)
+        btn_frame.pack(fill=X, padx=20, pady=20)
         self.btn_login = Button(btn_frame, text="Log in", bg="lightgreen", fg="black", width=15,
-                              font=('Helvetica', 11), command=self.login_func).grid(row = 5, column = 1)
+                              font=('Helvetica', 11), command=self.login_func).pack(side=LEFT, ipady=3)
         self.btn_reg = Button(btn_frame, text = 'Sign up',bg = 'lightgreen',fg = 'black',width = 15,
-                              font = ('Helvetica',11), command = self.reg_func).grid(row = 6, column = 1)
+                              font = ('Helvetica',11), command = self.reg_func).pack(side=RIGHT, ipady=3)
        
         self.login_frame.mainloop()
     
