de1760064474691868635b915d84ae0e8afcc11d
RebeccaWXY <58260839+RebeccaWXY@users.noreply.github.com>
2019-12-09 03:53:16 +0800

    Add files via upload

---
+++ client_main_modified.py
@@ -0,0 +1,115 @@
+import chat_client_class as chatClient
+from tkinter import messagebox
+import Login
+import Main
+from chat_utils import *
+import argparse
+import json
+#import MD5
+from threading import Thread
+import time
+
+
+#  chatting模式， 需要重写
+def start():
+    global client
+    # 从chat_cmdl_client搬来的, 为了args （argument）
+    parser = argparse.ArgumentParser(description='chat client argument')
+    parser.add_argument('-d', type=str, default=None, help='server IP addr')
+    args = parser.parse_args()
+
+    # 开始仿照chat_client_class的运行顺序
+    client = chatClient.Client(args)
+    client.init_chat()
+
+    global login_window
+    login_window = Login.LoginPanel(login, register, close_login_window)
+    login_window.show()
+
+def send_message():
+    print("send message:")
+    content = Main.main_window.get_send_text()
+    if content == "" or content == "\n":
+        print("empty message")
+        return
+    print(content)
+    
+    Main.main_window.clear_send_text()
+    client.send_message(content)
+
+def close_sk():
+    print("trying to disconnect socket")
+    client.quit()
+
+def close_main_window():
+    close_sk()
+    Main.main_window.main_window.destroy()
+
+def close_login_window():
+    close_sk()
+    login_window.login_window.destroy()
+
+
+# from login go to register
+def register():
+    from register_GUI import Register_UI
+    print("click register button")
+    login_window.close()
+    global reg_window
+    reg_window = Register_UI(close_reg_window, register_submit, close_reg_window)
+    reg_window.show()
+
+# close register and go back to login
+def close_reg_window():
+    reg_window.close()
+    global login_window
+    login_window = Login.LoginPanel(login, register, close_login_window)
+    login_window.show()
+
+# close login and goto main(chat)
+def goto_main_window(user):
+    login_window.close()
+    global main_window
+    main_window = Main.MainPanel()
+    #main_window = Main.MainPanel(user, send_message, close_main_window)
+    # 新开一个线程专门负责接收并处理数据 --------—-→这个需要吗？？
+    # Thread(target=recv_data).start()
+    main_window.show()
+
+def login():
+    print("click login button")
+    user, key = login_window.get_input()
+    # key = MD5.gen_md5(key)
+
+    if user == "" or key == "":
+        messagebox.showwarning(title="Hint", message="User name or password is empty.")
+        return
+    print("user: " + user + ", key: " + key)
+
+    # 需要新添login过程中check的步骤（client和servser端）
+    result = client.check_user(user, key)
+    if result == '0':
+        goto_main_window(user)
+        client.sm.set_state(S_LOGGEDIN)
+        client.sm.set_myname(user)
+    elif result == '1':
+        messagebox.showerror(title='Error', message='User name does not exist.')
+    elif result == '2':
+        messagebox.showerror(title="Error", message="User name or password is wrong.")
+
+# submit register form
+def register_submit():
+    print("start to register")
+    user, key = reg_window.get_input()
+    # 需要新添register申请的步骤（client和server端）
+    result = client.register_user(user, key)
+    if result == "0":
+        messagebox.showinfo("Success", "You have registered successfully.")
+        close_reg_window()
+    elif result == "1":
+        messagebox.showerror("Error", "This username is registered.")
+
+#def handle_command(command): 
+    
+if __name__ == "__main__":
+    start()
